2f703c40882a3df9095ff7b1feb1455716408e3e
daiwenbo88 <309569849@qq.com>
2016-05-26 23:40:06 +0800

    daiwenbo

--- app/src/main/java/com/weichuanwang/testgit/MainActivity.java
+++ app/src/main/java/com/weichuanwang/testgit/MainActivity.java
@@ -10,7 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new TextView(this);
+        new String();
+		/*haahalhahcalchhcl
+		ha
+		*/
+
     }
 
 }

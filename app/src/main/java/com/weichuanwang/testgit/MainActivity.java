499a0133a3d7201508c5447039a675a6f84d81ea
00zifeng00 <w840750145@qq.com>
2016-05-26 23:31:35 +0800

    44444444

--- app/src/main/java/com/weichuanwang/testgit/MainActivity.java
+++ app/src/main/java/com/weichuanwang/testgit/MainActivity.java
@@ -1,8 +1,8 @@
 package com.weichuanwang.testgit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Button(this);
+        new TextView(this);
     }
 
 }
